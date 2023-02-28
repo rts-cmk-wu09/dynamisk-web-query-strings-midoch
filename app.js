67b980b12e758f4fcbcb58fa88ce7b4864a3ad7a
mido <101448145+midoch@users.noreply.github.com>
2023-02-28 13:22:12 +0100

    Update app.js

--- app.js
+++ app.js
@@ -11,7 +11,10 @@ fetch("/data/destinations.json")
       li.innerHTML = `
       <a href="destination.html?id=${destination.id}">
       <img src="img/${destination.image}">
-      <h2>${destination.title}</h2>
+      <div class="place__info">
+      <h3>${destination.destination}</h3>
+      <p class="">More</p>
+      </div>
       </a>
       `;
     });
